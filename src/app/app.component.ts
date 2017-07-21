0d8c665d3289303475ca546eb35b7943f1102209
Brian Coleman <me@briancoleman.net>
2017-07-21 14:09:00 -0600

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  posts: Post[] = [];
+
+  constructor(private http: Http) {
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://jsonplaceholder.typicode.com/posts')
+      .map((response: Response) => response.json() as Post[])
+      .subscribe((posts: Post[]) => this.posts = posts);
+  }
+}
+
+interface Post {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MyInterceptor} from './http-interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: MyInterceptor,
+    multi: true,
+  }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
